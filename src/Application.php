91178a1f099edb2ea56dc8013cead730519d2622
omegacms <omegacms>
2023-11-05 17:36:58 +0100

    Initial Release

---
+++ src/Application.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Part of Omega CMS - Application Package
+ *
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ */
+
+/**
+ * @declare
+ */
+declare( strict_types = 1 );
+
+/**
+ * @namespace
+ */
+namespace Omega\Application;
+
+/**
+ * @use
+ */
+use function method_exists;
+use function session_start;
+use function session_status;
+use Throwable;
+use Dotenv\Dotenv;
+use Omega\Container\Container;
+use Omega\Http\Response;
+use Omega\Routing\Router;
+
+/**
+ * Base application class.
+ *
+ * This `Application` class represents the main entry point of the Omega framework.
+ * It manages the application's lifecycle, including configuration, routing, and 
+ * handling HTTP requests.
+ *
+ * @category    Omega
+ * @package     Omega\Application
+ * @link        https://omegacms.github.io
+ * @author      Adriano Giovannini <omegacms@outlook.com>
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version     1.0.0
+ */
+class Application extends Container
+{
+    use SingletonTrait;
+
+    /** 
+     * Run the application. 
+     * 
+     * This method starts and runs the OSPress application. It handles the 
+     * entire application lifecycle, including session management, configuration 
+     * setup, routing, and processing HTTP requests. 
+     * 
+     * @return Response Return an instance of Response representing the application's response. 
+     * @throws Throwable If an error occurs during application execution. 
+     */
+    public function run() : Response
+    {
+        if ( session_status() !== PHP_SESSION_ACTIVE ) {
+            session_start();
+        }
+
+        $basePath = $this->resolve( 'paths.base' );
+
+        $this->configure( $basePath );
+        $this->bindProviders( $basePath );
+
+        return $this->dispatch( $basePath );
+    }
+
+    /** 
+     * Configure the application. 
+     * 
+     * This method sets up the application's configuration by loading environment 
+     * variables from Dotenv. 
+     * 
+     * @param  string $basePath Holds the base path of the application. 
+     * @return void 
+     */
+    private function configure( string $basePath ) : void
+    {
+        $dotenv = Dotenv::createImmutable( $basePath );
+        $dotenv->load();
+    }
+
+    /** 
+     * Bind providers to the application. 
+     * 
+     * This method binds service providers to the application, allowing them 
+     * to register services and perform any necessary setup. 
+     * 
+     * @param  string $basePath The base path of the application. 
+     * @return void 
+     */
+    private function bindProviders( string $basePath ) : void
+    {
+        $providers = require "{$basePath}/config/providers.php";
+
+        foreach ( $providers as $provider ) {
+            $instance = new $provider;
+
+            if ( method_exists( $instance, 'bind' ) ) {
+                $instance->bind( $this );
+            }
+        }
+    }
+
+    /** 
+     * Dispatch the application. 
+     * 
+     * This method dispatches the application, including routing setup and 
+     * handling of HTTP requests. 
+     * 
+     * @param  string $basePath The base path of the application. 
+     * @return Response An instance of Response representing the application's response. 
+     * @throws Throwable If an error occurs during dispatching. 
+     */
+    private function dispatch( string $basePath ) : Response
+    {
+        $router = new Router();
+
+        $this->bind( Router::class, fn() => $router );
+
+        $routes = require "{$basePath}/routes/web.php";
+        $routes( $router );
+
+        $response = $router->dispatch();
+
+        if ( ! $response instanceof Response ) {
+            $response = $this->resolve( 'response' )->content( $response );
+        }
+
+        return $response;
+    }
+}
---
+++ src/SingletonTrait.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Part of Omega CMS - Application Package
+ *
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ */
+
+/**
+ * @declare
+ */
+declare( strict_types = 1 );
+
+/**
+ * @namespace
+ */
+namespace Omega\Application;
+
+/**
+ * @use
+ */
+use function get_called_class;
+use Omega\Application\Exceptions\SingletonException;
+
+/**
+ * Singleton trait.
+ *
+ * This `SingletonTrait`provides a convenient way to implement the Singleton design 
+ * pattern in PHP. When a class uses this trait, it ensures that only one instance 
+ * of the class is created and provides a static method to access that instance. 
+ * 
+ * Usage: 
+ * - Simply add `use Singleton;` to your class. 
+ * - Access the singleton instance using `YourClass::getInstance()`.
+ *
+ * @category    Omega
+ * @package     Omega\Application
+ * @link        https://omegacms.github.io
+ * @author      Adriano Giovannini <omegacms@outlook.com>
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version     1.0.0
+ */
+trait SingletonTrait
+{
+    /**
+     * Singleton instance.
+     *
+     * @var mixed $instances Holds the singleton instances.
+     */
+    private static mixed $instances;
+
+    /** 
+     * Get the singleton instance. 
+     * 
+     * This method returns the singleton instance of the class. If an instance 
+     * doesn't exist, it creates one and returns it. 
+     * 
+     * @return mixed Return the singleton instance. 
+     */
+    public static function getInstance() : mixed
+    {
+        $getCalledClass = get_called_class();
+
+        if ( ! isset( self::$instances[ $getCalledClass ] ) ) {
+            self::$instances[ $getCalledClass ] = new $getCalledClass();
+        }
+
+        return self::$instances[ $getCalledClass ];
+    }
+
+    /** 
+     * SingletonTrait private constructor. 
+     * 
+     * This constructor is declared as private to prevent external instantiation 
+     * of the class. The singleton instance should be accessed via `getInstance()`. 
+     * 
+     * @return void 
+     */
+    protected function __construct() {}
+
+    /** 
+     * Clone method. 
+     * 
+     * This method is overridden to prevent cloning of the singleton instance. 
+     * Cloning would create a second instance, which violates the Singleton pattern. 
+     * 
+     * @return void 
+     * @throws SingletonException If an attempt to clone the singleton is made. 
+     */
+    public function __clone() : void
+    {
+        throw new SingletonException(
+            'You can not clone a singleton.'
+        );
+    }
+
+    /** 
+     * Wakeup method. 
+     * 
+     * This method is overridden to prevent deserialization of the singleton instance. 
+     * Deserialization would create a second instance, which violates the Singleton pattern. 
+     * 
+     * @return void 
+     * @throws SingletonException If an attempt at deserialization is made. 
+     */
+    public function __wakeup() : void
+    {
+        throw new SingletonException(
+            'You can not deserialize a singleton.'
+        );
+    }
+
+    /** 
+     * Sleep method. 
+     * 
+     * This method is overridden to prevent serialization of the singleton instance. 
+     * Serialization would create a second instance, which violates the Singleton pattern. 
+     * 
+     * @return array Return the names of private properties in parent classes.
+     * @throws SingletonException If an attempt at serialization is made. 
+     */
+    public function __sleep() : array
+    {
+        throw new SingletonException(
+            'You can not serialize a singleton.'
+        );
+    }
+}
